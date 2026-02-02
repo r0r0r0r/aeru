915631e5b91b8f39779307ec6538059838a5427a
r0r0r0r <2005ravichaurasiya@gmail.com>
2026-02-03 01:24:08 +0530

    fix: Use non-null assertion (!) for Sanity env vars

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
-export const dataset = (process.env.NEXT_PUBLIC_SANITY_DATASET || 'production') as string;
-export const projectId = (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'e1jul805') as string;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 
 export const useCdn = false
