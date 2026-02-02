2c0fd3eb4bbc3982595da89962e89f825f0a232b
r0r0r0r <2005ravichaurasiya@gmail.com>
2026-02-02 22:51:54 +0530

    fix: Force type casting for Sanity env variables

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'e1jul805';
+export const dataset = (process.env.NEXT_PUBLIC_SANITY_DATASET || 'production') as string;
+export const projectId = (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'e1jul805') as string;
 
 export const useCdn = false
